5704671e2c78f74285e923d4f813e3a5bdd18e94
ke1kyu <105688133+ke1kyu@users.noreply.github.com>
2023-03-02 01:57:05 +0900

    Add files via upload

---
+++ tag2txt.js
@@ -0,0 +1,25 @@
+// ページから指定したタグのテキストを取得する関数
+function get_text() {
+    const extract_tags = ['h1', 'h2', 'h3', 'p']; // 入手したいタグ
+    let text = '';
+    for (let tag of extract_tags) {
+        const tags = document.getElementsByTagName(tag);
+        for (let i = 0; i < tags.length; i++) {
+            text += tags[i].textContent + '\n';
+        }
+    }
+    return text;
+}
+
+// ページから取得したテキストをファイルに出力する関数
+function saveTextToFile(text) {
+    const blob = new Blob([text], { type: 'text/plain' });
+    const anchor = document.createElement('a');
+    anchor.download = 'output.txt';
+    anchor.href = URL.createObjectURL(blob);
+    anchor.click();
+}
+
+// メイン処理
+const text = get_text();
+saveTextToFile(text);
